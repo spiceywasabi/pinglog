7b8612c504255302f9e8b7fb449c60d535d1e6ae
Wasabi Burns <69662763+spiceywasabi@users.noreply.github.com>
2023-09-05 11:09:02 -0700

    add date format and remove other graphs

--- pingplot.py
+++ pingplot.py
@@ -1,5 +1,4 @@
-# Ping Plot Tool (PPT)
-import os
+# Ping Plot Tool (PPT)import os
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,15 +7,15 @@ import matplotlib.dates as mdates
 
 parser = argparse.ArgumentParser(description='Ping Plotting Tool')
 parser.add_argument('--srcdir', type=str, required=True, help='Directory containing CSV files')
+parser.add_argument('--datefmt', type=str, default="%Y-%m-%d %H:%M:%S", required=False, help='The Date Format')
 args = parser.parse_args()
 
 if not os.path.exists(args.srcdir):
 	print(f"Error, the dir {args.srcdir} does not exist")
 	sys.exit(1)
+
 csv_files = [file for file in os.listdir(args.srcdir) if file.endswith('.csv')]
-
 plt.figure(figsize=(16, 8))
-
 combined_chart_data = []
 
 for csv_file in csv_files:
@@ -30,11 +29,8 @@ for csv_file in csv_files:
     plt.ylabel('Response Time (ms)')
     plt.title(f'Response Time Over Time - {csv_file}')
     plt.grid(True)
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M:%S"))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter(args.datefmt))
     
-    individual_chart_path = os.path.join(args.srcdir, f'{os.path.splitext(csv_file)[0]}.png')
-    plt.savefig(individual_chart_path)
-
     combined_chart_data.append((csv_file, df['Timestamp'], df['Response Time (ms)']))
 
 plt.subplot(len(csv_files) + 1, 1, len(csv_files) + 1)
@@ -46,7 +42,7 @@ plt.xlabel('Timestamp')
 plt.ylabel('Response Time (ms)')
 plt.title('Combined Response Time Over Time')
 plt.grid(True)
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M:%S"))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter(args.datefmt))
 plt.legend()
 
 combined_chart_path = os.path.join(args.srcdir, 'combined_chart.png')
